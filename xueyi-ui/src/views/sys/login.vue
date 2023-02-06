15f08ef16c56ecb255d6e828f0cc63a8a5ed3747
雪忆天堂 <1455668754@qq.com>
2023-02-06 13:51:57 +0800

    fix: 首页页签右键选择时不显示关闭左侧

--- xueyi-ui/src/views/sys/login.vue
+++ xueyi-ui/src/views/sys/login.vue
@@ -56,7 +56,7 @@
     </el-form>
     <!--  底部  -->
     <div class="el-login-footer">
-      <span>Copyright © 2022-2025 xueyitt.cn All Rights Reserved.</span>
+      <span>Copyright © 2022-2023 xueyitt.cn All Rights Reserved.</span>
     </div>
   </div>
 </template>
