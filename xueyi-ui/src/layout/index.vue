f565142d0cfe49f5991b1e6fb8278be3358d4b4b
雪忆天堂 <1455668754@qq.com>
2023-04-22 11:29:08 +0800

    fix: 优化固定头部页签滚动条被隐藏的问题

--- xueyi-ui/src/layout/index.vue
+++ xueyi-ui/src/layout/index.vue
@@ -77,6 +77,9 @@ export default {
     .el-scrollbar{
       height: 100%;
     }
+    ::v-deep .el-scrollbar__bar.is-vertical {
+      z-index: 10;
+    }
 
     ::v-deep .el-scrollbar__wrap {
       overflow-x: hidden;
