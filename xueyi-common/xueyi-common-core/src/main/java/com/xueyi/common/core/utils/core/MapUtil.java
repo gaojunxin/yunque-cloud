9bdce781444cec16f7863922843732ed24aa2980
雪忆 <1455668754@qq.com>
2024-07-26 18:20:41 +0800

    feat: 增加MapUtil补充方法注解

--- xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/core/MapUtil.java
+++ xueyi-common/xueyi-common-core/src/main/java/com/xueyi/common/core/utils/core/MapUtil.java
@@ -9,18 +9,42 @@ import java.util.Map;
  */
 public class MapUtil extends cn.hutool.core.map.MapUtil {
 
+    /**
+     * 非空判断
+     *
+     * @param map Map
+     * @return 结果
+     */
     public static boolean isNotNull(Map<?, ?> map) {
         return !isNull(map);
     }
 
+    /**
+     * 空判断
+     *
+     * @param map Map
+     * @return 结果
+     */
     public static boolean isNull(Map<?, ?> map) {
         return null == map;
     }
 
+    /**
+     * 是否不存在指定键判断
+     *
+     * @param map Map
+     * @return 结果
+     */
     public static <T> boolean notContainsKey(Map<T, ?> map, T key) {
         return !containsKey(map, key);
     }
 
+    /**
+     * 是否存在指定键判断
+     *
+     * @param map Map
+     * @return 结果
+     */
     public static <T> boolean containsKey(Map<T, ?> map, T key) {
         return isNotEmpty(map) && map.containsKey(key);
     }
