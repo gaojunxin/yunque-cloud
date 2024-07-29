b723d47dcb4ad5e345274b974b9327e1f97dbcd1
雪忆 <1455668754@qq.com>
2024-07-29 16:11:06 +0800

    chore(component): 优化EditableCell组件handleSubmit方法报错时增加warn日志显示

--- multi-ui/src/components/Table/src/components/editable/EditableCell.vue
+++ multi-ui/src/components/Table/src/components/editable/EditableCell.vue
@@ -17,6 +17,7 @@
   import { treeToList } from '@/utils/helper/treeHelper';
   import { Spin } from 'ant-design-vue';
   import { parseRowKey } from '../../helper';
+  import { warn } from '@/utils/log/LogUtil';
 
   export default defineComponent({
     name: 'EditableCell',
@@ -283,6 +284,7 @@
               });
             } catch (e) {
               result = false;
+              warn(e);
             } finally {
               spinning.value = false;
             }
