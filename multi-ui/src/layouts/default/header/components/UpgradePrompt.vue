9ea7fa7d1e3a9498b1c907a1f1a285883ea7e293
雪忆 <1455668754@qq.com>
2024-08-01 11:26:43 +0800

    feat: add upgrade prompt

---
+++ multi-ui/src/layouts/default/header/components/UpgradePrompt.vue
@@ -0,0 +1,22 @@
+<script setup lang="ts">
+  import { h } from 'vue';
+  import { Modal } from 'ant-design-vue';
+
+  Modal.info({
+    title: '新版本发布',
+    content: h('div', {}, [h('p', 'MultiSaas v3.3.4 预览版本已发布')]),
+    onOk() {
+      handleClick();
+    },
+    okText: '前往体验新版',
+  });
+
+  function handleClick() {
+    window.open('https://doc.xueyitt.cn', '_blank');
+  }
+</script>
+<template>
+  <div>
+    <a-button type="primary" @click="handleClick">前往体验新版</a-button>
+  </div>
+</template>
