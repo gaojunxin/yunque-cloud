b647a914d9db56d26f57648d77d48b710129e47f
雪忆 <1455668754@qq.com>
2024-08-16 14:24:09 +0800

    feat: 代码生成管理文档注释更新

--- xueyi-modules/xueyi-gen/src/main/java/com/xueyi/gen/controller/admin/AGenController.java
+++ xueyi-modules/xueyi-gen/src/main/java/com/xueyi/gen/controller/admin/AGenController.java
@@ -11,6 +11,7 @@ import com.xueyi.gen.domain.dto.GenTableColumnDto;
 import com.xueyi.gen.domain.dto.GenTableDto;
 import com.xueyi.gen.domain.query.GenTableColumnQuery;
 import com.xueyi.gen.domain.query.GenTableQuery;
+import io.swagger.v3.oas.annotations.Operation;
 import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.validation.annotation.Validated;
@@ -37,20 +38,16 @@ import java.util.List;
 @RequestMapping("/admin/gen")
 public class AGenController extends BGenController {
 
-    /**
-     * 查询代码生成列表
-     */
     @Override
     @GetMapping("/list")
+    @Operation(summary = "查询代码生成列表")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_LIST)")
     public AjaxResult list(GenTableQuery table) {
         return super.list(table);
     }
 
-    /**
-     * 查询数据库列表
-     */
     @GetMapping("/db/list")
+    @Operation(summary = "查询数据库列表")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_LIST)")
     public AjaxResult dataList(GenTableQuery table) {
         startPage();
@@ -58,10 +55,8 @@ public class AGenController extends BGenController {
         return getDataTable(list);
     }
 
-    /**
-     * 查询数据表字段列表
-     */
     @GetMapping(value = "/column/list")
+    @Operation(summary = "查询数据表字段列表")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_LIST)")
     public AjaxResult columnList(GenTableColumnQuery query) {
         startPage();
@@ -69,28 +64,22 @@ public class AGenController extends BGenController {
         return getDataTable(list);
     }
 
-    /**
-     * 查询代码生成详细
-     */
     @Override
     @GetMapping(value = "/{id}")
+    @Operation(summary = "查询代码生成详细")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_SINGLE)")
     public AjaxResult getInfo(@PathVariable Serializable id) {
         return super.getInfo(id);
     }
 
-    /**
-     * 查询代码生成详细 | 包含代码生成数据
-     */
     @GetMapping(value = "/sub/{id}")
+    @Operation(summary = "查询代码生成详细 | 包含代码生成数据")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_SINGLE)")
     public AjaxResult getInfoExtra(@PathVariable Serializable id) {
         return super.getInfo(id);
     }
 
-    /**
-     * 预览代码
-     */
+    @Operation(summary = "预览代码")
     @GetMapping("/preview/{tableId}")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_PREVIEW)")
     public AjaxResult previewMulti(@PathVariable("tableId") Long tableId) {
@@ -98,10 +87,8 @@ public class AGenController extends BGenController {
         return success(dataMap);
     }
 
-    /**
-     * 导入表结构（保存）
-     */
     @PostMapping("/importTable")
+    @Operation(summary = "导入表结构（保存）")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_IMPORT)")
     @Log(title = "代码生成", businessType = BusinessType.IMPORT)
     public AjaxResult importTableSave(@RequestParam("tables") String tables, @RequestParam(value = "sourceName", required = false) String sourceName) {
@@ -112,23 +99,19 @@ public class AGenController extends BGenController {
         return success();
     }
 
-    /**
-     * 代码生成修改
-     */
     @Override
     @PutMapping
+    @Operation(summary = "代码生成修改")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_EDIT)")
     @Log(title = "代码生成管理", businessType = BusinessType.UPDATE)
     public AjaxResult edit(@Validated({V_E.class}) @RequestBody GenTableDto table) {
         return super.edit(table);
     }
 
-    /**
-     * 生成代码（下载方式）
-     */
+    @GetMapping("/download/{tableId}")
+    @Operation(summary = "生成代码（下载方式）")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_CODE)")
     @Log(title = "代码生成", businessType = BusinessType.GEN_CODE)
-    @GetMapping("/download/{tableId}")
     public void downloadMulti(HttpServletResponse response, @PathVariable("tableId") Long tableId) throws IOException {
         byte[] data = baseService.downloadCode(tableId);
         genCode(response, data);
@@ -137,6 +120,7 @@ public class AGenController extends BGenController {
     /**
      * 生成代码（自定义路径）
      */
+    @Operation(summary = "生成代码（自定义路径）")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_CODE)")
     @Log(title = "代码生成", businessType = BusinessType.GEN_CODE)
     @GetMapping("/generate/{tableId}")
@@ -145,22 +129,18 @@ public class AGenController extends BGenController {
         return success();
     }
 
-    /**
-     * 批量生成代码
-     */
+    @GetMapping("/batchGenCode")
+    @Operation(summary = "批量生成代码")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_CODE)")
     @Log(title = "代码生成", businessType = BusinessType.GEN_CODE)
-    @GetMapping("/batchGenCode")
     public void batchMultiGenCode(HttpServletResponse response, @RequestParam Long[] ids) throws IOException {
         byte[] data = baseService.downloadCode(ids);
         genCode(response, data);
     }
 
-    /**
-     * 代码生成批量删除
-     */
     @Override
     @DeleteMapping("/batch/{idList}")
+    @Operation(summary = "代码生成批量删除")
     @PreAuthorize("@ss.hasAuthority(@Auth.GEN_GENERATE_DEL)")
     @Log(title = "代码生成管理", businessType = BusinessType.DELETE)
     public AjaxResult batchRemove(@PathVariable List<Long> idList) {
