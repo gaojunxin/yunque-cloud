cc299bf62f4b385baa4b3a36c5287a2ddc048ab7
雪忆 <1455668754@qq.com>
2024-08-17 11:42:21 +0800

    feat: 内部构造请求也正常传递请求头设定

--- xueyi-common/xueyi-common-security/src/main/java/com/xueyi/common/security/feign/FeignRequestInterceptor.java
+++ xueyi-common/xueyi-common-security/src/main/java/com/xueyi/common/security/feign/FeignRequestInterceptor.java
@@ -2,7 +2,6 @@ package com.xueyi.common.security.feign;
 
 import com.xueyi.common.core.constant.basic.SecurityConstants;
 import com.xueyi.common.core.context.SecurityContextHolder;
-import com.xueyi.common.core.utils.core.ObjectUtil;
 import com.xueyi.common.core.utils.core.StrUtil;
 import com.xueyi.common.core.utils.ip.IpUtil;
 import com.xueyi.common.core.utils.servlet.ServletUtil;
@@ -25,29 +24,29 @@ public class FeignRequestInterceptor implements RequestInterceptor {
     @Override
     public void apply(RequestTemplate requestTemplate) {
         HttpServletRequest httpServletRequest = ServletUtil.getRequest();
-        if (ObjectUtil.isNotNull(httpServletRequest)) {
-            Map<String, String> headers = ServletUtil.getHeaders(httpServletRequest);
-            // 传递用户信息请求头，防止丢失
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.ENTERPRISE_ID);
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.ENTERPRISE_NAME);
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.IS_LESSOR);
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.USER_ID);
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.USER_NAME);
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.NICK_NAME);
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.USER_TYPE);
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.SOURCE_NAME);
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.STRATEGY_ID);
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.ACCOUNT_TYPE);
+        Map<String, String> headers = Optional.ofNullable(httpServletRequest).map(ServletUtil::getHeaders).orElse(null);
+        // 传递用户信息请求头，防止丢失
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.ENTERPRISE_ID);
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.ENTERPRISE_NAME);
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.IS_LESSOR);
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.USER_ID);
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.USER_NAME);
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.NICK_NAME);
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.USER_TYPE);
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.SOURCE_NAME);
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.STRATEGY_ID);
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.ACCOUNT_TYPE);
 
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.ACCESS_TOKEN.getCode());
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.USER_KEY.getCode());
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.AUTHORIZATION_HEADER.getCode());
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.SUPPLY_AUTHORIZATION_HEADER.getCode());
-            setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.TENANT_IGNORE.getCode());
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.ACCESS_TOKEN.getCode());
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.USER_KEY.getCode());
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.AUTHORIZATION_HEADER.getCode());
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.SUPPLY_AUTHORIZATION_HEADER.getCode());
+        setHeaderKey(requestTemplate, headers, SecurityConstants.BaseSecurity.TENANT_IGNORE.getCode());
 
+        Optional.ofNullable(httpServletRequest).ifPresent(request -> {
             // 配置客户端IP
             requestTemplate.header("X-Forwarded-For", IpUtil.getIpAddr());
-        }
+        });
     }
 
     /**
@@ -72,8 +71,6 @@ public class FeignRequestInterceptor implements RequestInterceptor {
      * @param headerKey       请求头键值
      */
     private void setHeaderKey(RequestTemplate requestTemplate, Map<String, String> headers, String headerKey) {
-        String key = headers.get(headerKey);
-        if (StrUtil.isNotBlank(key))
-            requestTemplate.header(headerKey, key);
+        Optional.ofNullable(headers).map(map -> map.get(headerKey)).filter(StrUtil::isNotBlank).ifPresent(info -> requestTemplate.header(headerKey, info));
     }
 }
