798dc275507b0c1afd5ceba933572d9e2b3423b7
gaojunxin <gaojunxin2283@pansoft.com>
2024-09-10 00:32:58 +0800

    feature：恢复多数据源模块

--- xueyi-common/xueyi-common-datasource/src/main/java/com/xueyi/common/datasource/config/MyDynamicDataSourceConfig.java
+++ xueyi-common/xueyi-common-datasource/src/main/java/com/xueyi/common/datasource/config/MyDynamicDataSourceConfig.java
@@ -5,8 +5,6 @@ import com.baomidou.dynamic.datasource.processor.DsJakartaSessionProcessor;
 import com.baomidou.dynamic.datasource.processor.DsProcessor;
 import com.baomidou.dynamic.datasource.processor.DsSpelExpressionProcessor;
 import com.baomidou.dynamic.datasource.spring.boot.autoconfigure.DynamicDataSourceAutoConfiguration;
-import com.xueyi.common.datasource.processor.DsIsolateExpressionProcessor;
-import com.xueyi.common.datasource.processor.DsMasterExpressionProcessor;
 import org.springframework.boot.autoconfigure.AutoConfigureBefore;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,12 +23,8 @@ public class MyDynamicDataSourceConfig {
         DsJakartaHeaderProcessor headerProcessor = new DsJakartaHeaderProcessor();
         DsJakartaSessionProcessor sessionProcessor = new DsJakartaSessionProcessor();
         DsSpelExpressionProcessor spelExpressionProcessor = new DsSpelExpressionProcessor();
-        DsIsolateExpressionProcessor isolateExpressionProcessor = new DsIsolateExpressionProcessor();
-        DsMasterExpressionProcessor masterExpressionProcessor = new DsMasterExpressionProcessor();
         headerProcessor.setNextProcessor(sessionProcessor);
         sessionProcessor.setNextProcessor(spelExpressionProcessor);
-        spelExpressionProcessor.setNextProcessor(isolateExpressionProcessor);
-        isolateExpressionProcessor.setNextProcessor(masterExpressionProcessor);
         return headerProcessor;
     }
 }
